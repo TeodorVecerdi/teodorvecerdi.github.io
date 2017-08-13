058d1015224677de575f6a6684ff7c9757a2a959
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-08-14 00:07:17 +0300

    Replace address bar text on file change

--- js/main.js
+++ js/main.js
@@ -2,9 +2,5 @@
  * Created by Teodor Vecerdi on 13/08/17.
  */
 $(function () {
-    if (history.replaceState) {
-        window.history.replaceState("object or string", "Teodor Vecerdi", "");
-    } else {
-        document.location.href = "";
-    }
+    window.location.hash = '';
 });
