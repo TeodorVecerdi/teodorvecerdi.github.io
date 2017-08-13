6e6e7afbe87a6e830f72ca0871772ef0f9986fb3
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-08-14 00:16:21 +0300

    Replace address bar text on file change

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@
  * Created by Teodor Vecerdi on 13/08/17.
  */
 $(function () {
-    window.location.hash = '';
+    $("a.language").click(function(e) {
+        e.preventDefault(); //so the browser doesn't follow the link
+
+        $("#page-top").load(this.href, function() {
+            //execute here after load completed
+        });
+    });
 });
