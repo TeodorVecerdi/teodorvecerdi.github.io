06634fac370bb4b8296a319b6814576744fe4664
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-08-14 00:39:55 +0300

    Replace address bar text on file change (7)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(function () {
     $("a.language").click(function(e) {
         e.preventDefault();
         $.get(this.href + " body", function(data) {
-            $("body").replaceWith(data);
+            $("body").replaceWith(data, function () {
+                $("#languagesDropdown").dropdown();
+            });
         });
     });
 });
