d076066bec01e41ed8a1bc7b247015c3376cb34b
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-08-14 00:05:00 +0300

    Replace address bar text on file change

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@
  * Created by Teodor Vecerdi on 13/08/17.
  */
 $(function () {
-    location.pathname = "www.teodorvecerdi.me";
+    if (history.replaceState) {
+        window.history.replaceState("object or string", "Teodor Vecerdi", "");
+    } else {
+        document.location.href = "";
+    }
 });
