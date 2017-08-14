9b56d3e402b72dac5d322d20ea4fb6de4dca54d3
TeodorVecerdi <teo.vecerdi@gmail.com>
2017-08-14 16:14:22 +0300

    Change language system to use json lang files instead of multiple html files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 $(function () {
-    setAddress("");
+
+    var e = localStorage.getItem("lang");
+    if(!e)
+        e = "en";
+    setLanguage(e);
+    $("a.language").click(function (e) {
+        e.preventDefault();
+        var lang = $(this).prop("lang");
+        console.log(lang);
+        setLanguage(lang);
+    })
 });
 
 function setAddress(b) {
     var a = history.state;
     history.pushState(a, "", b);
+}
+
+function setLanguage(langCode) {
+    localStorage.setItem("lang", langCode);
+    var lang={};
+    $.getJSON('../lang/'+langCode+'.json', function(data) {
+        lang=data;
+    });
+    $.each(lang, function (key, value) {
+       $("#"+key).text(value);
+       console.log(key + ": " + value);
+    });
 }
